9553ba363313e6a917f782ab55679978a5c46d36
gemtara <reliveall@gmail.com>
2015-12-24 16:23:09 +0000

    upload again

--- hello-cpp-world.cc
+++ hello-cpp-world.cc
@@ -1,5 +1,19 @@
 #include <iostream>
 using namespace std;
+
+class machine {
+public:
+  machine(){
+      cout<<"new"<<endl;
+  };  
+  ~machine() {
+      cout<<"del"<<endl;
+  };
+  
+};
+
 int main() {
+    machine *machine1 = new machine();
     cout << "Hello World! Richard" << std::endl;
+    delete machine1;
 }
